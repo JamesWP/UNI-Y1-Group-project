b4de6e5206a141208a67c03c9bedd61a1d2b1a34
James Peach <james.peach@student.manchester.ac.uk>
2014-03-01 00:29:56 +0000

    finished question logic

--- examples/questionViewer/module-question.js
+++ examples/questionViewer/module-question.js
@@ -12,23 +12,75 @@ Question.prototype.isCorrect = function(){
 }
 Question.answerDataTag = 'answer';
 Question.prototype.init = function(){
-    $answersDiv = $('<div>').addClass("answers");
-    switch(this.data.type){
+    var question = this;
+    var $answersDiv = $('<div>').addClass("answers");
+    switch(question.data.type){
       case "multiplechoice":
-        $.each(this.data.answers,function(answer){
-          $answer = $("<div>").addClass("answer");
-          $answer.text(this.text);
+        $.each(question.data.answers,function(){
+          var answer = this;
+          var $answer = $("<div>").addClass("answer");
+          $answer.text(answer.text);
           $answer.data(Question.answerDataTag,answer);
+          $answer.click(function(){
+            $answersDiv.children().removeClass("selected");
+            $answer.addClass("selected");
+            question.correct = answer.correct;
+          });
           $answersDiv.append($answer);
         });
         break;
+      case "truefalse":
+        var $trueAnswer = $("<div>").addClass("answer").text("True");
+        var $falseAnswer = $("<div>").addClass("answer").text("False");
+        var trueFalseClick = function(){
+          $answersDiv.children().removeClass("selected");
+          $(this).addClass("selected");
+          question.correct = ( ($trueAnswer[0]==this) == question.data.answer );            
+        }
+        $falseAnswer.click(trueFalseClick);
+        $trueAnswer.click(trueFalseClick);
+        $answersDiv.append($trueAnswer);
+        $answersDiv.append($falseAnswer);
+        break;
+      case "blanks":
+        var $input = $("<input>").attr("type","text"); 
+        $input.keyup(function(){
+          var answer = $(this).val();
+          var curmindist = 99999;
+          var correct = true;
+          $.each(question.data.answers,function(){
+            var dist = Question.editDistance(this.text,answer);
+            curmindist = dist<curmindist?dist:curmindist;
+            if(answer==this.text&&!this.correct){
+              correct = false;
+              return false;
+            }
+          });
+          if(!correct)
+            question.correct = false;
+          else
+            question.correct = curmindist<=question.data.editdistance;
+        });
+        $answersDiv.append($input);
+        break;
+      default:
+        throw new Error("type not recognised");
     }
     this.$questionEl.append($answersDiv);
 };
 Question.prototype.getQuestionEl = function (){ return this.$questionEl};
-
 var QuestionTemplateManager = function(templateID){
   this.questionTemplate = Handlebars.compile($('#'+templateID).html());
+}
+Question.editDistance = function(s,t){
+  if (!s.length) return t.length;
+  if (!t.length) return s.length;
+
+  return Math.min(
+    Question.editDistance(s.substr(1), t) + 1,
+    Question.editDistance(t.substr(1), s) + 1,
+    Question.editDistance(s.substr(1), t.substr(1)) + (s[0] !== t[0] ? 1 : 0)
+  );
 }
 QuestionTemplateManager.prototype.loadModel = function(model){
   return new Question($(this.questionTemplate(model)),model);
