ba0421de5c17311aa6236279f35c2e642378beba
Francesca Popescu <iolanda.popescu@student.manchester.ac.uk>
2014-03-12 13:51:33 +0000

    refined signup

--- websiteroot/signup-page.php
+++ websiteroot/signup-page.php
@@ -5,8 +5,8 @@
 <div class="container">
     <div class="col-md-4 list-group">
     </div>
-<form action ="../database/create-account.php" method = "post" class="col-md-4 form-group" style="align: center;">
-<h2 class="form-signin-heading" class="">Sign up for a free account</h2>
+<form action ="../database/create-account.php" method = "post" class="col-md-4 form-group" style="align: center; margin-top: 50px;">
+<h2 class="form-signin-heading" style="margin-bottom: 50px;">Sign up for a free account</h2>
     <div class="form-group">
       <label for="name">Username</label>
       <input type="text" class="form-control" id="user" placeholder="Username">
@@ -15,7 +15,7 @@
       <label for="password">Password</label>
       <input type="password" class="form-control" id="password" placeholder="Password">
     </div>  
-   <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 30px;">Sign up</button>
+   <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 60px; margin-bottom: 50px;">Sign up</button>
 </form>
 
 </div>
