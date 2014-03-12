d4eeaded009386fb11090071dd2987084ab60a8a
Yuli Chen <yuli.chen@student.manchester.ac.uk>
2014-03-12 16:08:55 +0000

    flip up

--- websiteroot/result.php
+++ websiteroot/result.php
@@ -1,46 +1,37 @@
 <?php include '../application/app.php'; ?>
-<?php pageInit(); ?>
-<?php pageHead(); ?>
+<?php include '../database/connect.php'; ?>
+<?php //pageInit(); ?>
 
-<h2 ba> Well Done! Your score is: X.</h2>
-     <div class="row">
-      <div class="col-md-6">
-        <a class='list-group-item active'>Results</span></a>
-        <a class='list-group-item'>result1<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result2<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result3<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result4<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result5<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result6<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result7<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result8<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result9<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result10<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result11<span class="pull-right btn btn-danger"></a>
-        <a class='list-group-item'>result12<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result13<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result14<span class="pull-right btn btn-success"></a>
-        <a class='list-group-item'>result15<span class="pull-right btn btn-success"></a>
-      </div>
-      <div class="col-md-2">
-        <a class='list-group-item active'>Other People's scores</span></a>
-        <a class='list-group-item'>01<span class="pull-right "></a>
-        <a class='list-group-item'>02<span class="pull-right "></a>
-        <a class='list-group-item'>03<span class="pull-right "></a>
-        <a class='list-group-item'>04<span class="pull-right "></a>
-        <a class='list-group-item'>05<span class="pull-right "></a>
-        <a class='list-group-item'>06<span class="pull-right "></a>
-        <a class='list-group-item'>07<span class="pull-right "></a>
-        <a class='list-group-item'>08<span class="pull-right "></a>
-        <a class='list-group-item'>09<span class="pull-right "></a>
-        <a class='list-group-item'>10<span class="pull-right "></a>
-        <a class='list-group-item'>11<span class="pull-right "></a>
-        <a class='list-group-item'>12<span class="pull-right "></a>
-        <a class='list-group-item'>13<span class="pull-right "></a>
-        <a class='list-group-item'>14<span class="pull-right "></a>
-        <a class='list-group-item'>15<span class="pull-right "></a>
+<?php /* TODO need to gather data from database */
+  $Questions = array();
+  $Questions[] = array("title"=>"Question 1","correct"=> true,"text"=>"example text1");
+  $Questions[] = array("title"=>"Question 2","correct"=> false,"text"=>"example text2");
+?>
+
+<?php pageHead(); ?>
+  <div class="container">
+    <div id=flipup-logo class="row">
+      <div class="col-md-8 col-md-offset-2 pagination-centered">
+        <img class="img-responsive" src="<?php echo getBaseUrl();?>images/flipup.png"/> 
       </div>
     </div>
-   <button href="#">Go back to Question Page <i class="icon-heart"></i></button>
-
+    <div class="row">
+      <div class="col-md-12 list-group">
+        <a href="#" class='list-group-item active'>Question</span></a>
+        <?php foreach($Questions as $Question){ ?>
+          <div class="panel-group" id="accordion">
+	  	        <div class="panel panel-default">
+	  	          <div class="panel-heading"> 
+          		       <a href="<?php echo getLinkForSubject($Question['correct'])?>" class='list-group-item'><?php echo $Question['title'];?> <span class="pull-right btn btn-success"></a>
+		          	<div class="panel-collapse collapse">
+		            	  <div class="panel-body">
+		              	    test
+		            	  </div>
+		          	</div>
+                           </div>
+                     	</div>                    
+        <?php } ?>
+      </div>
+    </div>
+  </div>
 <?php pageFoot(); ?>
