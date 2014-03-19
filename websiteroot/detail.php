df651bb032bace5b7cb38e77edd422176eacafd9
reem.althani <reem.althani@student.manchester.ac.uk>
2014-03-19 02:42:09 +0000

    new file:   websiteroot/detail.php 	new file:   websiteroot/detail1.php 	new file:   websiteroot/help.php

---
+++ websiteroot/detail.php
@@ -0,0 +1,34 @@
+<?php include '../application/app.php'; ?>
+<?php pageInit(); ?>
+
+<?php pageHead(); ?>
+
+      <div class="container">
+        <div class="jumbotron"> 
+      <div class="row">
+        <div class="col-lg-12">
+        <h2><font color="#428BCA">How can I create my own deck of questions?</font></h2>
+        <p>You need to be logged in to create your own page. Then go to (the create your own) button on the top of the page, choose a subject, and you could generate the questions by typing the question, choosing the type of answers, typing the answers depends on the type. You cannot write less than 10 questions per deck.</p>
+        <h2><font color="#428BCA">How can I modify my information?</font></h2>
+        <h2><font color="#428BCA">How can I share my result with my friends if I do not have an account?</font></h2>
+        <h2><font color="#428BCA">How can I register to the website?</font></h2>
+        <p>Go to register button in the top of the main page, and you could register by choosing a user name, password, and an email.</p>
+        <h2><font color="#428BCA">How can I improve my result?</font></h2>
+        <p>You could improve your result by reading more on the topic and by solving as many quizzes as it required.</p>
+        <h2><font color="#428BCA">Why is it different?</font></h2>
+        <h2><font color="#428BCA">What if I forget my password?</font></h2>
+        <p>There is a forget your password button you could click on it in the login page and that will send your password to your account.</p>
+        <h2><font color="#428BCA">What if I decided to change the subject in the middle of the quiz?</font></h2>
+        <p>You could always go to the subject button on the top of the page and choosing another subject.</p>
+        <h2><font color="#428BCA">How can I choose a subject?</font></h2>
+        <p>There would be the list of subjects on the main page, or alternatively you could choose the subject button on the top of the page.</p> 
+        <h2><font color="#428BCA">How can a quiz generate?</font></h2>
+        <p>It generate randomly from a deck of questions.</p>
+        <h2><font color="#428BCA">How can I know what decks are there?</font></h2>
+        <p>You could know the top rated decks from the main page, or you could choose a subject, and all the decks in the subject will be listed.</p>
+        <a class="btn btn-primary" href="Help.html" role="button">Go back to Help us Page &raquo;</a></p>
+       </div>
+     </div>
+    </div>
+   </div>
+<?php pageFoot(); ?>      
---
+++ websiteroot/help.php
@@ -0,0 +1,44 @@
+<?php include '../application/app.php'; ?>
+<?php pageInit(); ?>
+
+<?php pageHead(); ?>
+      <div class="container">
+        <div class="jumbotron">
+          <h1><font color="#428BCA">Help us</font></h1>
+          <p class="lead">This page is going to help user to know more things about this website and how this website works and what you can do in the website.</p>
+          <div class="row">
+            <div class="col-lg-6">
+              <h2><font color="#428BCA">What is "Flip Up"</font></h2>
+              <p class="text">Flipup is a quiz website based around flip-cards, commonly used for revision. We made this website so that you can use it as both a source of revision and entertainment, allowing you to create, share and take quizzes.</p>
+              <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+          <div class="col-lg-6">
+            <h2><font color="#428BCA">FAQ-Frequently Asked Questions</font></h2>
+            <p>How can I create my own deck of questions?</p>
+            <p>How can I modify my information?</p>
+            <p>How can I share my result with my friends if I do not have an account?</p>
+            <p>How can I register to the website?</p>
+            <p>How can I improve my result?</p>
+            <p>Why is it different?</p>
+            <p>What if I forget my password?</p>
+            <p>What if I decided to change the subject in the middle of the quiz?</p>
+            <p>How can I choose a subject?</p>
+            <p>How can a quiz generate?</p>
+            <p>How can I know what decks are there?</p>
+            <p><a class="btn btn-primary" href="detail.php" role="button">View details &raquo;</a></p>
+         </div>
+         <div class="col-lg-6">
+          <h2><font color="#428BCA">Terms and Conditions</font></h2>
+          <p>contents</p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+         </div>
+         <div class="col-lg-6">
+          <h2><font color="#428BCA">Data and Privacy</font></h2>
+          <p>Accessing websites is a good way to keep in touch with the society and acquire some knowledge. People can create anything they want like text, images and other elements and potentially share thousands of opinions around the world, so there are some ethical issues appeared. Whether you realize it or not, they must take responsibility for these.</p>
+          <p>For example, In our website "Flip up", we create a system which lets people take a quiz made up a few of the questions in our database, or simply add their own in order to improve the existing sections. However, in order to avoid any law or ethical issues, so both the user and us, the developers, should have responsibilities:</p>
+          <p><a class="btn btn-primary" href="detail1.php" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+    </div> 
+  </div>
+<?php pageFoot(); ?>      
