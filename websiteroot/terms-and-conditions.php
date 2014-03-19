4cab4abfcba345197c0a1d8196168d7095c9e76f
Isabel Whistlecroft <isabel.whistlecroft@student.manchester.ac.uk>
2014-03-19 23:37:58 +0000

    Changed most of the content on the terms and conditions page.

--- websiteroot/terms-and-conditions.php
+++ websiteroot/terms-and-conditions.php
@@ -7,14 +7,57 @@
       <div class="row">
         <div class="col-lg-12">
           <h2 class="text-center"><font color="#428BCA">Terms &amp Conditions</font></h2>
-          <p>Any content submitted by a use become to property of Flipup and may be altered or removed without notice.</p>
-          <p> Any content submitted to Flipup must not be subject to copyright laws. </p>
-          <p> Flipup reserves the right to remove any content it deems offensive or unsuitable without any given notice. </p>
-          <p> Features on the website may be subject to change without any given notice. </p>
-          <p> Any members who repeatedly post offensive, inappropriate or copyrighted content may have their website membership withdrawn. </p>
-          <a class="btn btn-primary" href="about.php" role="button">Go back to Help us Page &raquo;</a></p>
+          <p>These terms of use govern your use of our site. These website terms of use have been provided and, approved by the solicitors at legal documents resource DIY Legals. Please read these terms in full before you use this Website. If you do not accept these terms of use, please do not use this Website. Your continued use of this site confirms your acceptance of these terms.</p>
+          <h3> Web Site Access </h3>
+          <p>It is not necessary to register with us in order to use most parts of this Website. However, particular areas of this Website will only be accessible only if you have registered. </p>
+          <h3>Use of Website</p>
+          <ul>
+            <li>This website may be used for your own private purposes and in accordance with these terms of use.</li>
+            <li>You may print and download material from this website provided that you do not modify or reproduce the content without our prior written consent.</li>
+          </ul>
+          <h3>Site Uptime</h3>
+          <ul>
+            <li>All reasonable measures are taken by us to ensure that this website is in operation most of the time. However, occasionally technical issues may result in some downtime and accordingly we will not be liable if this website is unavailable at any time.</li>
+            <li>Where possible we always try to give advance warning of maintenance issues that may result in Website down time but we shall not be obliged to provide such notice.</li>
+          </ul>
+          <h3>Visitor Provided Material</h3>
+          <ul>
+            <li>Any material that a visitor to this Website sends or posts to this Website shall be considered non-proprietary and non confidential. We shall be entitled to copy, disclose, distribute or use for such other purpose as we deem appropriate all material provided to us, with the exception of personal information, the use of which is covered under our Privacy Policy [insert hyperlink here].</li>
+            <li>When using this website you shall not post or send to or from this Website any material:
+              <ul>
+                <li>(a) for which you have not obtained all necessary consents; </li>
+                <li>(b) that is discriminatory, obscene, pornographic, defamatory, liable to incite racial hatred, in breach of confidentiality or privacy, which may cause annoyance or inconvenience to others, which encourages or constitutes conduct that would be deemed a criminal offence, give rise to a civil liability, or otherwise is contrary to the law in the United Kingdom;</li>
+                <li>(c) which is harmful in nature including, and without limitation, computer viruses, Trojan horses, corrupted data, or other potentially harmful software or data.</li>
+              </ul>
+            </li>
+          </ul>
+          <h3>Links To and From Other Websites</h3>
+          <ul>
+            <li>1.9 Throughout this Website you may find links to third party websites. The provision of a link to such a website does not mean that we endorse that website.  If you visit any website via a link on this Website you do so at your own risk.</li>
+            <li>1.10 Any party wishing to link to this website is entitled to do so provided that the conditions below are observed:
+              <ul>
+                <li>(a) you do not seek to imply that we are endorsing the services or products of another party unless this has been agreed with us in writing;</li>
+                <li>(b) you do not misrepresent your relationship with this website; and</li>
+                <li>(c) the website from which you link to this Website does not contain offensive or otherwise  controversial content or, content that infringes any intellectual property rights or other rights of a third party.</li>
+              </ul>
+            </li>
+            <li>1.11 By linking to this Website in breach of clause 5.2 you shall indemnify us for any loss or damage suffered to this Website as a result of such linking.</li>
+          </ul>
+          <h3>Disclaimer</h3>
+          <ul>
+            <li>Whilst we do take all reasonable steps to make sure that the information on this website is up to date and accurate at all times we do not guarantee that all material is accurate and ,or up to date.</li> 
+            <li>All material contained on this Website is provided without any or warranty of any kind. You use the material on this Website at your own discretion.</li>
+          </ul>
+          <h3>Exclusion of Liability</h3>
+          <ul>
+            <li>We do not accept liability for any loss or damage that you suffer as a result of using this Website.</li>
+            <li>Nothing in these Terms of Use shall exclude or limit liability for death or personal injury caused by negligence which cannot be excluded or under the law of the United Kingdom.</li>
+          </ul>
+          <h3>Law and Jurisdiction</h3>
+          <p>These terms of use are governed by English law. Any dispute arising in connection with these terms of use shall be subject to the exclusive jurisdiction of the Courts of England and Wales.</p>
+          <a class="btn btn-primary" href="about.php" role="button">Back</a></p>
        </div>
      </div>
     </div>
    </div>
-<?php pageFoot(); ?> 
+<?php pageFoot(); ?>
