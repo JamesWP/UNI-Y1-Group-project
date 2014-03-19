1c53795a301f73cd4f130a3cbd932d371631cf86
Francesca Popescu <iolanda.popescu@student.manchester.ac.uk>
2014-03-19 03:57:10 +0000

    pretty question viewer

--- websiteroot/quiz.php
+++ websiteroot/quiz.php
@@ -62,4 +62,27 @@
       $selectedItem.addClass('active');
     });
   </script>
+  <script type="text/javascript">
+    colours = [
+      {name: 'green', color:'#beffaa', border:'#90ee90', active:'#5cb85c'},
+      {name: 'pink', color:'#ffd1dc', border:'#ff85a2', active:'#ff9eb5'},
+      {name: 'yellow', color:'#fdfdaf', border:'#fefec8', active:'#fdfd7d'},
+      {name: 'blue', color:'#d4ebf2', border:'#86c5da', active:'#99d0e0'},
+      {name: 'orange', color:'#ffddae', border:'#ffb347', active:'#ffc97b'},
+      {name: 'purple', color:'#cabbcb', border:'#9c819f', active:'#9c819f'},
+      {name: 'gray', color:'#c6c6c6', border:'#adadad', active:'#adadad'}
+    ];
+
+    var randomColour = colours[Math.floor(Math.random() * colours.length)];
+    $(".questionCont").css("background", randomColour.color);
+    $(".questionCont .active").css("background", randomColour.active);
+    $(".questionCont .list-group-item").hover(
+      function () {
+        $(this).css("background", randomColour.active);
+      }, 
+      function () {
+        $(this).not(".active").css("background", '#fff');
+      }
+    );
+</script>
 <?php pageFoot(); ?>
