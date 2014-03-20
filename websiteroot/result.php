e64d1e1a16148db595f4920788c6848cdd11a1fc
James Peach <james.peach@student.manchester.ac.uk>
2014-03-20 01:08:01 +0000

    got quizzes working

--- websiteroot/result.php
+++ websiteroot/result.php
@@ -4,7 +4,7 @@
 <?php /* TODO need to gather data from database */
   $score = 5;
   connectDB();
-  $data = getResults(15);
+  $data = getResults($_SESSION['quizID']);
   $questions = $data['questions'];
   $correct = $data['correct'];
   $otherResults = array(array('user' => 'username', 'score' => 'score'));
@@ -28,15 +28,13 @@
 
     <div class="col-md-6 list-group">
       <a class="list-group-item active">
-
       Question Results
-
       </a>
       <?php foreach($questions as $question){ ?>
         <div class="accordion" id="accordion2">
           <div class="accordion-group">
             <div class="accordion-heading">
-      		    <a href='#' class='list-group-item'><?php echo $question['title'];?> <span class="pull-right btn btn-<?php echo $question['correct']?"success":"danger"; ?>" ></span></a>
+              <a href='#' class='list-group-item'><?php echo $question['title'];?> <span class="pull-right btn btn-<?php echo $question['correct']?"success":"danger"; ?>" ></span></a>
           	  <div class="panel-collapse collapse">
             	  <div class="panel-body"><?php echo $question['text']; ?></div>
           	  </div>
@@ -46,27 +44,29 @@
       <?php } ?>
     </div>
     <div class="col-md-6">
-      <div class="list-group">
-      <a class="list-group-item active">
-
-     Other's Results
-
-      </a>
-        <?php foreach($otherResults as $otherResult){ ?>
-        		    <a href='#' class='list-group-item'><?php echo $otherResult['user'];?> <span class="pull-right"><?php echo $otherResult['score'];?></span></a>
-        <?php } ?> 
+      <div class="row">
+        <div class="list-group">
+          <a class="list-group-item active">
+            Other's Results
+          </a>
+          <?php foreach($otherResults as $otherResult){ ?>
+            <a href='#' class='list-group-item'><?php echo $otherResult['user'];?> <span class="pull-right"><?php echo $otherResult['score'];?></span></a>
+          <?php } ?>
+        </div>
       </div>
-      <div class="list-group">
-      <a class="list-group-item active">
-
-      Other Quizes
-
-      </a>
-        <?php foreach($otherResults as $otherResult){ ?>         
-        		    <a href='#' class='list-group-item'><?php echo $relatedQuizes['otherQuizes'];?></a>
-        <?php } ?> 
+      <div class="row">
+        <div class="list-group">
+          <a class="list-group-item active">
+            Other Quizes
+          </a>
+          <?php foreach($otherResults as $otherResult){ ?>
+            <a href='#' class='list-group-item'><?php echo $relatedQuizes['otherQuizes'];?></a>
+          <?php } ?>
+        </div>
       </div>
    </div>
+ </div>
+</div>
 <script>
  $('.list-group-item').click(function(){
    $(this).parent().find('.collapse').toggle()
