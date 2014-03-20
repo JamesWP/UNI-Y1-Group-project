3519eacb0f359c213adbfa1e6770a16e0ff46566
reem.althani <reem.althani@student.manchester.ac.uk>
2014-03-20 14:07:28 +0000

    decks page

---
+++ websiteroot/decks.php
@@ -0,0 +1,57 @@
+<?php include '../application/app.php'; ?>
+<?php pageInit(); ?>
+
+<?php pageHead(); ?>
+
+<?php
+// $id = $_GET['id'];
+connectDB();
+  $decks = getDecks(2);
+  $subjectInfo = getSubjectInfo(2);
+  $deckID = 2;
+  $questions = getQuestions($deckID);
+  $deckInfo = getDeckInfo($deckID);
+disconnectDB();
+?>
+  <div class="container">
+    <div class="page-header">
+      <h1>The list of decks for <?php echo $subjectInfo["name"]; ?></h1>
+    </div>
+     <div class="row">
+       <div class="col-md-10"></div>
+       <div class="col-md-2"><button type="button" class="btn btn-default">create new question</button></div>
+     </div>
+    <div class="row">
+      <div class="col-md-6 list-group">
+          <a class='list-group-item active'>List of Decks</a>
+          <?php foreach($decks as $deck){?>
+            <a href="<?php echo getQuizLinkForDeck($deck['id']);?>" class="list-group-item"><?php echo $deck["name"]; ?></a>
+        <?php } ?>
+      </div>
+      <div class="col-md-6 list-group">
+          <a class='list-group-item active'> Questions for <?php echo $deckInfo["name"]; ?> </a>
+          <?php foreach($questions as $question){?>
+            <div href="#" class='list-group-item'>
+              <div class="row">
+                <div class="col-xs-9">
+                  <?php echo $question["text"]; ?>
+                </div>
+                <div class="col-xs-3">
+                  <button href="<?php echo getBaseUrl()."create-your-own.php? deckID=". $question['id']; ?>" type="button" class="btn btn-warning btn-sm">edit</button>
+                  <button href="#" type="button" class="btn btn-danger btn-sm">delete</button>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+          
+      </div>
+    </div>
+  </div>
+<script>
+
+  $('button[href]').click(function(){
+  	window.location.href = $(this).attr('href');
+  })
+  
+</script>
+<?php pageFoot(); ?>      
